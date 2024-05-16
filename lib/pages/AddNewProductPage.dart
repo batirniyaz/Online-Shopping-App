9cf825868a044727bbe7f51c78516a53dcf99e58
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-05-16 21:52:59 +0500

    add leading iconbutton "go back"

--- lib/pages/AddNewProductPage.dart
+++ lib/pages/AddNewProductPage.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+class AddNewProductPage extends StatelessWidget {
+  AddNewProductPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            context.go("/home");
+          },
+        ),
+        title: Text('Add New Product'),
+      ),
+      body: Center(
+        child: Text('Add New Product Page'),
+      ),
+    );
+  }
+}
+
