a46ac5adfb18d18ab97224397e734135c0e534d1
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-05-16 16:47:45 +0500

    made configuration on LoginPage file

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  List<String> products = [
+    'Product 1',
+    'Product 2',
+    'Product 3',
+    'Product 4',
+    'Product 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Home Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Home Page",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                context.go('/login');
+              },
+              child: Icon(Icons.add),
+              backgroundColor: Colors.green,
+            ),
+    );
+  }
+}
